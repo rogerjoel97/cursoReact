8ed68cdc018d37e8014986b30ad3a721c13ba21b
rogerjhl <roger.hernandez@sitiouno.com>
2021-02-23 21:04:04 -0400

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function HelloWorld(props) {
+  console.log(props)
+  return(
+    <div id = 'hello'>{props.mytext}</div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component: <HelloWorld mytext = "Hello Fazt"/> 
+      <HelloWorld mytext = "Hola mundo"/> 
+      <HelloWorld mytext = "hello!"/> 
     </div>
   );
 }
